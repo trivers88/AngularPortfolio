e5b26119fb741de0d8f27dc43896af3b70e464e5
Travis Rivers <travrivers88@gmail.com>
2019-04-10 15:14:17 +0000

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/<travportfolio2>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<travportfolio2>/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080)
