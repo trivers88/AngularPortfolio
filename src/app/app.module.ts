f1d6532aadedd2aaea784fcce5248793aefb7e5f
Trav <travrivers88@gmail.com>
2020-10-12 18:57:07 -0600

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
-import { HomeComponent } from './home/home.component';
-import { DogsComponent } from './dogs/dogs.component';
-import { AppsComponent } from './apps/apps.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { LandingComponent } from './landing/landing.component';
-import { CirclesComponent } from './apps/circles/circles.component';
-import { TodoListComponent } from './apps/todo-list/todo-list.component';
-import { ColorGuesserComponent } from './apps/color-guesser/color-guesser.component';
-
+import { AppComponent } from "./app.component";
+import { AppHeaderComponent } from "./app-header/app-header.component";
+import { HomeComponent } from "./home/home.component";
+import { DogsComponent } from "./dogs/dogs.component";
+import { AppsComponent } from "./apps/apps.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { LandingComponent } from "./landing/landing.component";
+import { CirclesComponent } from "./apps/circles/circles.component";
+import { TodoListComponent } from "./apps/todo-list/todo-list.component";
+import { ColorGuesserComponent } from "./apps/color-guesser/color-guesser.component";
 
 const appRoutes: Routes = [
-  {path: '', component: LandingComponent },
-  {path: 'home', component: HomeComponent },
-  {path: 'dogs', component: DogsComponent },
-  {path: 'apps', component: AppsComponent},
-  {path: 'circles', component: CirclesComponent },
-  {path: 'todo', component: TodoListComponent },
-  {path: 'colorguesser', component: ColorGuesserComponent },
-  {path: 'aboutme', component: AboutMeComponent }]
-  
+  { path: "", component: LandingComponent },
+  { path: "home", component: HomeComponent },
+  { path: "dogs", component: DogsComponent },
+  { path: "apps", component: AppsComponent },
+  { path: "circles", component: CirclesComponent },
+  { path: "todo", component: TodoListComponent },
+  { path: "colorguesser", component: ColorGuesserComponent },
+  { path: "aboutme", component: AboutMeComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
+    BrowserAnimationsModule,
     HomeComponent,
     DogsComponent,
     AppsComponent,
@@ -38,12 +39,9 @@ const appRoutes: Routes = [
     TodoListComponent,
     ColorGuesserComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppModule { }
+export class AppModule {}
